7c17593a08b2bbf266853f36b0392f1cb889b73f
Andrew Horvath <skwerl@student.fullsail.edu>
2022-11-03 14:45:01 -0500

    setting up python portion of assignment

--- wk2_assignments/assignment_1/python/Grader.py
+++ wk2_assignments/assignment_1/python/Grader.py
@@ -1,6 +1,13 @@
 class Grader:
-    def __init__(self, grade, feedback):
-        self.grade = grade
+    def __init__(self, gradeLetter, feedback):
+        self.grade = gradeLetter
         self.feedback = feedback
 
-        def grade()
+        def get_lettergrade(self, gradeInput):
+            if gradeInput >= 90:
+                gradeLetter = "A"
+                feedback = "You completed the requirements for the assignment, Great Job!"
+            else:
+                feedback = "Please try again."
+
+                return [gradeLetter, feedback]
