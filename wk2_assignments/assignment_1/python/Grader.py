35166c5c1a399dbfb92d99c06e3a0f43a5ba1126
Andrew Horvath <skwerl@student.fullsail.edu>
2022-11-03 21:57:21 -0500

    finished up python part.

--- wk2_assignments/assignment_1/python/Grader.py
+++ wk2_assignments/assignment_1/python/Grader.py
@@ -1,13 +1,23 @@
 class Grader:
-    def __init__(self, gradeLetter, feedback):
-        self.grade = gradeLetter
-        self.feedback = feedback
-
-        def get_lettergrade(self, gradeInput):
-            if gradeInput >= 90:
+        def lettergrade(self, gradeInput):
+            gradeNum = float(gradeInput)
+            gradeLetter = ""
+            feedback = ""
+            if gradeNum >= 90:
                 gradeLetter = "A"
                 feedback = "You completed the requirements for the assignment, Great Job!"
+            elif gradeNum >= 80:
+                gradeLetter = "B"
+                feedback = "You have a few small errors in your code."
+            elif gradeNum >= 70:
+                gradeLetter = "C"
+                feedback = "You are missing a requirement(s) for the assignment"
+            elif gradeNum >= 60:
+                gradeLetter = "D"
+                feedback = "You are missing some requirements(s) for the assignment, and have errors."
             else:
-                feedback = "Please try again."
-
-                return [gradeLetter, feedback]
+                gradeLetter = "F"
+                feedback = "You did not fulfill the requirements for the assignment! Try Again."
+            
+            gradeObj = {"gradeLetter": gradeLetter, "feedback": feedback}
+            return gradeObj
--- wk2_assignments/assignment_1/python/horvath_andrew_python.py
+++ wk2_assignments/assignment_1/python/horvath_andrew_python.py
@@ -1,2 +1,14 @@
 import Grader
+import sys
+
+name = raw_input("What is your name? ")
+assignment = raw_input("What assignment is this for? ")
+grade = raw_input("What is your grade for " + assignment + "? ")
+
+obj = Grader.Grader()
+returnData = obj.lettergrade(grade)
+
+print(name + "@fullsail.edu")
+print ("Your grade for " + assignment + " is: " + returnData["gradeLetter"] )
+print (returnData["feedback"])
 
