ae82f11b4e385ebb786c0dfcd0a9a4718dfad131
Andrew Horvath <skwerl@student.fullsail.edu>
2022-11-03 23:35:57 -0500

    finished up ruby portion of assignment

--- wk2_assignments/assignment_1/ruby/Grader.rb
+++ wk2_assignments/assignment_1/ruby/Grader.rb
@@ -7,6 +7,15 @@ class Grader
         if gradeNum >= 90
             gradeLetter = "A"
             feedback = "You completed the requirements for the assignment, Great Job!"
+        elsif gradeNum >= 80
+                gradeLetter = "B"
+                feedback = "You have a few small errors in your code."
+        elsif gradeNum >= 70
+                gradeLetter = "C"
+                feedback = "You are missing a requirement(s) for the assignment"
+        elsif gradeNum >= 60
+                gradeLetter = "D"
+                feedback = "You are missing some requirements(s) for the assignment, and have errors."
         else
             gradeLetter = "F"
             feedback = "You did not fulfill the requirements for the assignment! Try Again."
