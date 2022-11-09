f67f9113917bcf6d5e5c089e836e4a11ece42141
Andrew Horvath <skwerl@student.fullsail.edu>
2022-11-09 13:34:20 -0600

    setting up week 3 examples

--- wk3_examples/wk3_example_node_requests/app.js
+++ wk3_examples/wk3_example_node_requests/app.js
@@ -1,43 +1,32 @@
+"use strict"
 const fs = require('fs');
 const http = require('http');
 const path = require('path');
 const url = require('url');
 
-http.createServer(function(req, res){
-    var parsed = url.parse(req.url);
-    var filename = path.parse(parsed.pathname);
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-    //ternary conditionals to check file names, extension and directory paths
-    filen = filename.name==""?"index":filename.name;
-    ext = filename.ext==""?".html":filename.ext;
-    dir = filename.dir=="/"?"":filename.dir+"/";
-    page = filename.name==""?"index.html":filename.name;
-    
-    //removing the backslash at the beginning of the directory as it is not needed
-    f = (dir+filen+ext).replace("/","");
+let ejs = require('ejs');
+const app = express();
+const router = express.Router();
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
 
-    //checking the file extension and what type of file it is
-    var mimeTypes = {
-        '.html': ['text/html',""],
-        '.js': ['text/javascript',""],
-        '.css': ['text/css','utf8'],
-        '.png': ['image/png','Base64'],
-        '.jpg': ['image/jpeg','Base64'],
-        ".ico": ['image/x-icon','Base64'],
-        '.gif': ['image/gif','Base64'],
-    };
-
-    if(f){
-        fs.readFile(f, function(err,data){
-                res.writeHead(200, {'Content-Type': mimeTypes[ext][0]});
-                    //checking the fiel extension, adding the script to html files for navigation highlighting
-                    if(ext==".html"){
-                        res.write("<script>var page='"+filen+"';</script>")
-                    }
-                       res.end(data, mimeTypes[ext][1]);
-        })
-    }
-
-}).listen("8080", function(){
-    console.log("info", "Server is running on port : " + 8080);
+router.get('/', (req, res) => {
+    res.render('index',{pagename: 'Home'});
 })
+
+router.get('/about', (req, res) => {
+    res.render('about',{pagename: 'About'});
+})
+
+
+
+
+app.use(express.static('public'));
+app.use('/', router);
+const server = app.listen('8080');
