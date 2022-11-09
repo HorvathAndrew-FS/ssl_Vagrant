035c33844d8f093388a7dc311dd6db55b32f49e1
Andrew Horvath <skwerl@student.fullsail.edu>
2022-11-09 11:12:25 -0600

    added comments to app.js

--- wk2_assignments/assignment_2/app.js
+++ wk2_assignments/assignment_2/app.js
@@ -7,13 +7,16 @@ http.createServer(function(req, res){
     var parsed = url.parse(req.url);
     var filename = path.parse(parsed.pathname);
 
+    //ternary conditionals to check file names, extension and directory paths
     filen = filename.name==""?"index":filename.name;
     ext = filename.ext==""?".html":filename.ext;
     dir = filename.dir=="/"?"":filename.dir+"/";
     page = filename.name==""?"index.html":filename.name;
     
+    //removing the backslash at the beginning of the directory as it is not needed
     f = (dir+filen+ext).replace("/","");
 
+    //checking the file extension and what type of file it is
     var mimeTypes = {
         '.html': ['text/html',""],
         '.js': ['text/javascript',""],
@@ -27,12 +30,11 @@ http.createServer(function(req, res){
     if(f){
         fs.readFile(f, function(err,data){
                 res.writeHead(200, {'Content-Type': mimeTypes[ext][0]});
-                    
+                    //checking the fiel extension, adding the script to html files for navigation highlighting
                     if(ext==".html"){
                         res.write("<script>var page='"+filen+"';</script>")
                     }
                        res.end(data, mimeTypes[ext][1]);
-                       console.log(mimeTypes[ext][0]);
         })
     }
 
