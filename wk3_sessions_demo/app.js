088026511549f6365b06bcaadaee25cd8037c0ab
Andrew Horvath <skwerl@student.fullsail.edu>
2022-11-15 18:48:19 -0600

    wrkn on not auth user err

--- wk3_sessions_demo/app.js
+++ wk3_sessions_demo/app.js
@@ -27,6 +27,7 @@ app.use("/", router);
 
 //global use variable 
 let sess;
+let errors = [];
 
 const port = 8080;
 app.listen(port, () => {
@@ -59,10 +60,10 @@ router.get('/register', (req, res) => {
 
 router.get("/profile", (req, res) => {
     sess = req.session;
-    console.log(sess.loggedIn);
+    console.log("profile", sess.loggedIn);
     if(typeof(sess) == "undefined" || sess.loggedIn != true){
        
-        let errors = ["Not Authenticated User!"];
+        errors.push = ("Not Authenticated User!");
         res.render("pages/index", {pagename: 'Home', errors: errors});
 
     }else{
@@ -72,7 +73,6 @@ router.get("/profile", (req, res) => {
 
 router.get('/logout', (req, res) => {
     sess = req.session;
-    // delete req.session;
     req.session.destroy((err) => {
      if(err) {
        return console.log(err);
@@ -83,7 +83,7 @@ router.get('/logout', (req, res) => {
 
 //validation and routing for small login form
 router.post('/login', (req, res) => {
-    let errors = [];
+    
     if(req.body.email == ""){
         errors.push('Email is required');
     }
@@ -95,9 +95,13 @@ router.post('/login', (req, res) => {
     }
     if(!/^([a-zA-Z0-9@*#]{5,15})$/.test(req.body.password)){
         errors.push('Password is not valid!');
-    }
-    
-    if(req.body.email === "mike@aol.com" && req.body.password === "Abc123"){
+    }    
+    if(req.body.email != "mike@aol.com" && req.body.password != "Abc123"){
+        sess = req.session;
+        sess.email = req.body.email;
+        sess.loggedIn = false;
+        res.redirect('/profile');
+    }else{
         sess = req.session;
         console.log(req.body);
         console.log(sess);
@@ -105,9 +109,8 @@ router.post('/login', (req, res) => {
         sess.loggedIn = true;
         console.log(req.session)        
         res.render('pages/profile', {pagename: 'Profile', errors: errors, sess: sess});
-    }else{
-        res.render('pages/index', {pagename: 'Home', errors:errors, sess: sess})   
     }
+   
 })
 
 //validation and routing for registration form
